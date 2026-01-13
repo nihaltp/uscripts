66bb474b27931f832250f4a7c13eb17272bf5598
NIHAL  T P <nihaltpnki@gmail.com>
2026-01-13 23:18:50 +0530

    Add W3Schools Next Question Auto-Continue script

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # W3Schools
 
-Auto-continue on w3schools
+[Auto-continue on w3schools lessons after clicking "Next" button](https://raw.githubusercontent.com/nihaltp/uscripts/main/w3schools/auto_continue.user.js)
 
-[w3schools auto-continue](https://raw.githubusercontent.com/nihaltp/uscripts/main/w3schools/auto_continue.user.js)
+[Auto-continue on w3schools quizzes after clicking "Submit Answer" button](https://raw.githubusercontent.com/nihaltp/uscripts/main/w3schools/next_question.user.js)
---
+++ w3schools/next_question.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         W3Schools Next Question Auto-Continue
+// @description  Auto-click the "Next Question" button on W3Schools quiz modals after clicking "Submit Answer".
+// @author       nihaltp
+// @namespace    https://github.com/nihaltp/uscripts
+// @supportURL   https://github.com/nihaltp/uscripts/issues
+// @homepageURL  https://github.com/nihaltp/uscripts
+// @homepage     https://github.com/nihaltp/uscripts
+// @license      MIT
+// @match        https://www.w3schools.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=w3schools.com
+// @version      1.0.0
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/nihaltp/uscripts/main/w3schools/next_question.user.js
+// @downloadURL  https://raw.githubusercontent.com/nihaltp/uscripts/main/w3schools/next_question.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function waitForContinueAndClick() {
+        const btn = document.querySelector('button.ws-btn[onclick="goto_next_question()"]');
+        if (btn) {
+            console.log('[TM] Clicking Next Question modal Continue');
+            btn.click();
+        } else {
+            requestAnimationFrame(waitForContinueAndClick);
+        }
+    }
+
+    function hookNextButton() {
+        const submitBtn = document.querySelector(
+            'button#answerbutton[onclick="submit_answer()"]'
+        );
+
+        if (!submitBtn) {
+            requestAnimationFrame(hookNextButton);
+            return;
+        }
+
+        submitBtn.addEventListener('click', () => {
+            console.log('[TM] Submit Answer clicked, waiting for Next Question modal...');
+            waitForContinueAndClick();
+        }, true);
+    }
+
+    hookNextButton();
+})();
